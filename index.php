0842c877073decfbddf12e9b74b65bdbb7f0d95d
mpstratcons <mp.stratcons@proton.me>
2025-05-08 12:18:00 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Site Test IA</title>
+</head>
+<body>
+  <h1>Bienvenue !</h1>
+  <p>Ce site est généré et hébergé automatiquement.</p>
+</body>
+</html>
